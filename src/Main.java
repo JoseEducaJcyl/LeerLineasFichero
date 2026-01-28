4df4881c5006a62eff46940799c9a393f030a99f
Jose Antonio Morales Gutierrez <josea.morgut.1@educa.jcyl.es>
2026-01-28 12:48:57 +0100

    Mejorando contado palabras y caracteres

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,20 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         int contador = 0;
+        int caracteres = 0;
+        int palabras = 0;
         try (BufferedReader reader = new BufferedReader(new
                 FileReader("datos.txt"))) {
             String linea;
             while ((linea = reader.readLine()) != null) {
                 contador++;
                 System.out.println(linea);
+                caracteres += linea.length();
+                palabras += linea.split(" ").length;
             }
-            System.out.println("Contador: " + contador);
+            System.out.println("\nNumero de lineas: " + contador);
+            System.out.println("\nNumero de caracteres: " + caracteres);
+            System.out.println("\nNumero de palabras: " + palabras);
         } catch (IOException e) {
             System.out.println("Ocurrió un error al leer el archivo: " +
                     e.getMessage());
