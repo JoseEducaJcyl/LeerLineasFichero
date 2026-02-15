4a7bd3744a1d069a38fdef20fb4800a976864037
JoseEducaJcyl <josea.morgut.1@educa.jcyl.es>
2026-02-15 16:20:50 +0100

    Comentarios agregados

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,22 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        //Declaramos variables para contar, palabras, caracteres y lineas
         int contador = 0;
         int caracteres = 0;
         int palabras = 0;
+        //Leemos un fichero de texto, en este caso "datos.txt"
         try (BufferedReader reader = new BufferedReader(new
                 FileReader("datos.txt"))) {
             String linea;
+            //Se recorre cada linea, y se cuentas las lineas, palabras y caracteres 
             while ((linea = reader.readLine()) != null) {
                 contador++;
                 System.out.println(linea);
                 caracteres += linea.length();
                 palabras += linea.split(" ").length;
             }
+            //Se muestra los resultados 
             System.out.println("\nNumero de lineas: " + contador);
             System.out.println("\nNumero de caracteres: " + caracteres);
             System.out.println("\nNumero de palabras: " + palabras);
